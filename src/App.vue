de68e7407e84b5dbd54720ef9754bb73c7101d5a
gabikiwi <gabriel.chivoiu@gmail.com>
2017-10-29 19:19:32 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,18 @@
 <template>
   <div>
-    <h1>{{title}}</h1>
-    <p>{{greeting()}}</p>
-    <archive></archive>
+
   </div>
 </template>
 
 <script>
-import Client from './Archive.vue'
-
 export default {
-  components: {
-    archive:Client
-  },
-  name: 'app',
-  data () {
-    return {
-      title: 'Client Application!'
-    }
-  },
-  methods:{
-    greeting:function(){
-      return 'hey there!';
-    }
-
+  name: "app",
+  data() {
+    return {};
   }
-}
-  
-
+};
 </script>
 
-<style scoped>
-h1{
-  color: blueviolet;
-}
+<style >
+
 </style>
